73e26914e93697775f12b236159b64d90d3ccfeb
Jose Manuel <jose.galonso.2010@gmail.com>
2024-01-15 19:35:57 +0100

    primeraParte

---
+++ src/cajero.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class cajero {
+
+    public static void main(String[] args) {
+        //Vamos a crear la primera parte del proyecto
+
+        Scanner sc = new Scanner(System.in);
+
+        //atributos
+        boolean salir = false;
+        double cantidad = 1000.00;
+
+
+        while(salir == false){
+
+            System.out.println("tienes un par de opciones, que quieres hacer");
+            System.out.println("1. Retirar fondos");
+            System.out.println("2. Ingresar fondos");
+            System.out.println("3. Salir");
+            int opcion = sc.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Dime cuantos quieres retirar");
+                    System.out.println("Tienes " + cantidad);
+                    break;
+                case 2:
+                    System.out.println("Dime cuanto quieres ingresar");
+                    double anadir = sc.nextDouble();
+                    System.out.println("Vas a añadir, " + anadir);
+                    cantidad += anadir;
+                    System.out.println("Tienes en total " + cantidad);
+                    break;
+                case 3 :
+                    System.out.println("Adios");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("no has introducido un numero correctamente");
+            }
+        }
+
+    }
+
+}
